d4aca9f9fdadab55e232a43d41432f0da981b9b8
trent-i <98287324+trent-i@users.noreply.github.com>
2022-02-25 13:04:07 -0700

    stored userID in variable from homepage URL

--- routes/home.js
+++ routes/home.js
@@ -7,6 +7,11 @@ const router = express.Router()
 router.get("/", function(req, res) {
     res.render("home")
     console.log('directed user to homepage.')
+
+    var userID = req.query.userid
+    console.log("userID from homepage: ", userID)
+
+    // use the userID variable to query db for pet list 
 })
 
 
