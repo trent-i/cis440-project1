03f3175ac53a587e48ef2531cadd48377d38ac07
tanner.burch@gmail.com <tanner.burch@gmail.com>
2022-02-26 13:10:44 -0700

    home screen loads based on the user accessing it, so there

--- routes/add_pet.js
+++ routes/add_pet.js
@@ -9,7 +9,36 @@ router.get('/', function(req, res) {
 })
 
 router.post('/', function(req, res) {
-    res.send('add code to connect to database then add pet to database.')
+    // console.log(req.body.firstName)
+    
+    con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+    });
+
+    const data = req.body;
+  
+  let Fname    =  data.firstName
+  let Lname    =  data.lastName
+  let Email    =  data.email
+  let Password =  data.password
+  let query =`INSERT INTO Users ( Fname,Lname, Email, Password ) VALUES
+  ( '${Fname}', '${Lname}', '${Email}', '${Password}');`
+
+    // This is the data from the create account page input felds it's in json format)
+    console.log("Data: ", data)
+
+  // querys the database
+  con.query(query, (err,rows) => {
+    if(err) throw err;
+  
+    console.log('Data received from Db:');
+    console.log(rows);
+    res.json(rows)
+  });
+
+  con.end();
+
 })
 
 app.use(cors());
